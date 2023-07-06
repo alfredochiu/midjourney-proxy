8a03a1e5e9d8ace463924de90e5f472375092c0d
alfredochiu <121852243+alfredochiu@users.noreply.github.com>
2023-07-06 20:40:56 +0800

    new

--- src/main/java/com/github/novicezk/midjourney/controller/SubmitController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/SubmitController.java
@@ -193,7 +193,7 @@ public class SubmitController {
 		} catch (MalformedURLException e) {
 			return SubmitResultVO.fail(ReturnCode.VALIDATION_ERROR, "base64格式错误");
 		}
-		log.debug(dataUrlList)
+		// log.debug(dataUrlList)
 		Task task = newTask(blendDTO);
 		task.setAction(TaskAction.BLEND);
 		task.setDescription("/blend " + task.getId() + " " + dataUrlList.size());
